22497e495c5d5fcdb8f7a39aa52a90bb40d5afc9
piliwilliam0306 <piliwilliam0306@gmail.com>
2017-03-21 11:51:54 +0800

    add upload port in ini file

---
+++ sanity-test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# upload using ArduinoISP
+import argparse
+import os
+import time
+
+def upload():
+    os.system('python upload.py 328p_left rugby-328-blink')	
+    time.sleep(1)
+    os.system('python upload.py 328p_right rugby-328-blink')
+    time.sleep(1)
+    os.system('python upload.py 328p_pan rugby-328-blink')
+    time.sleep(1)
+    os.system('python upload.py 328p_tilt rugby-328-blink')
+    time.sleep(1)
+    os.system('python upload.py 328p_imu rugby-328-blink')
+    time.sleep(1)
+    os.system('python upload.py 328p_follow rugby-328-blink')
+    time.sleep(1)
+    os.system('python upload.py mega_wheel rugby-2560-blink')
+    time.sleep(1)
+    os.system('python upload.py mega_head rugby-2560-blink')
+
+if __name__ == '__main__':
+	upload()
