3410002e7e1e7fcfd0d3749dcde55a0d787ed4b6
piliwilliam0306 <piliwilliam0306@gmail.com>
2017-03-22 10:43:03 +0800

    add fuse set script

--- fuse-set.py
+++ fuse-set.py
@@ -3,27 +3,37 @@
 import argparse
 import os
 import time
+#fuse328p = 'avrdude -P /dev/ttyUSB5 -b 19200 -c avrisp -p m328p -U efuse:w:0x05:m -U hfuse:w:0xDA:m -U lfuse:w:0xFF:m'
+#fuse2560 = 'avrdude -P /dev/ttyUSB5 -b 19200 -c avrisp -p m2560 -U efuse:w:0xFD:m -U hfuse:w:0xD8:m -U lfuse:w:0xFF:m'
 
 def upload():
-    os.system('platformio run -d ArduinoISP/328p_left')
-    os.system(328p_fuse)	
-    os.system('platformio run -d ArduinoISP/328p_right')	
-    os.system(328p_fuse)	
-    os.system('platformio run -d ArduinoISP/328p_pan')	
-    os.system(328p_fuse)	
-    os.system('platformio run -d ArduinoISP/328p_tilt')	
-    os.system(328p_fuse)	
-    os.system('platformio run -d ArduinoISP/328p_imu')	
-    os.system(328p_fuse)	
-    os.system('platformio run -d ArduinoISP/328p_follow')	
-    os.system(328p_fuse)	
-    os.system('platformio run -d ArduinoISP/mega_wheel')
-    os.system(2560_fuse)	
-    os.system('platformio run -d ArduinoISP/mega_head')
-    os.system(2560_fuse)	
+    os.system('platformio run -d ArduinoISP/328p_left --target upload')
+    os.system(fuse328p)	
+    os.system(fuse328p)	
+    os.system('platformio run -d ArduinoISP/328p_right --target upload')	
+    os.system(fuse328p)	
+    os.system(fuse328p)	
+    os.system('platformio run -d ArduinoISP/328p_pan --target upload')	
+    os.system(fuse328p)	
+    os.system(fuse328p)	
+    os.system('platformio run -d ArduinoISP/328p_tilt --target upload')	
+    os.system(fuse328p)	
+    os.system(fuse328p)	
+    os.system('platformio run -d ArduinoISP/328p_imu --target upload')	
+    os.system(fuse328p)	
+    os.system(fuse328p)	
+    os.system('platformio run -d ArduinoISP/328p_follow --target upload')	
+    os.system(fuse328p)	
+    os.system(fuse328p)	
+    os.system('platformio run -d ArduinoISP/mega_wheel --target upload')
+    os.system(fuse2560)	
+    os.system(fuse2560)	
+    os.system('platformio run -d ArduinoISP/mega_head --target upload')
+    os.system(fuse2560)	
+    os.system(fuse2560)	
 
 if __name__ == '__main__':
-    328p_fuse = 'avrdude -P /dev/ttyUSB5 -b 19200 -c avrisp -p m328p -U efuse:w:0x05:m -U hfuse:w:0xDE:m -U lfuse:w:0xFF:m'
-    2560_fuse = 'avrdude -P /dev/ttyUSB5 -b 19200 -c avrisp -p m2560 -U efuse:w:0xFD:m -U hfuse:w:0xD8:m -U lfuse:w:0xFF:m'
+    fuse328p = 'avrdude -P /dev/ttyUSB5 -b 19200 -c avrisp -p m328p -U efuse:w:0x05:m -U hfuse:w:0xDA:m -U lfuse:w:0xFF:m'
+    fuse2560 = 'avrdude -P /dev/ttyUSB5 -b 19200 -c avrisp -p m2560 -U efuse:w:0xFD:m -U hfuse:w:0xD8:m -U lfuse:w:0xFF:m'
     upload()
 
